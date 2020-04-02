86fff0b08f789d82a28962c27eb30faab396f9b9
helaviol <viola.helaakoski@helsinki.fi>
2020-04-02 19:55:44 +0300

    README.md

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,20 @@
-function count_files {
-        count=$(ls -l | grep -v ^d | wc -l)
-        echo $count
+echo Welcome. 
+echo ""
+function howmanyfiles {
+	echo How many files are in the current directory?
+	read response
+files=$(ls -1 | wc -l)
 }
-
-echo How many files are in the current directory?
-
-read response
-
-count=$(count_files)
-
-if [[ $response -ne $count ]]
-then
-        while [ $response -ne $count ]
-        do
-                if  [[ $response -lt $count ]]
-                then
-                        echo "Your answer is incorrect. Aim a little higher!"
-                elif [[ $response -gt $count ]]
-                then
-                        echo "Your answer is incorrect. Aim a little lower!"
-                fi
-                read response
-        done
-fi
-echo "Congratulations! Your answer is correct"
+howmanyfiles
+while [[ $response -ne $files ]]
+do
+	if [[ $response -lt $files ]]
+	then
+		echo Your answer is incorrect. Aim a little higher!
+	elif [[ $response -gt $files ]]
+	then
+		echo Your answer is incorrect. Aim a little lower!
+	fi
+	howmanyfiles
+done
+echo Congratulations! Your answer is correct. 
